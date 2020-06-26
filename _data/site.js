1373b17b3868874517ac658cd40ed420177b27b2
Richard Herbert <richard@infoweb.co.uk>
2020-06-26 09:49:39 +0100

    bug: move Maps API key to .env file

--- _data/site.js
+++ _data/site.js
@@ -1,5 +1,7 @@
 let siteurl = 'https://www.classicbarber.co.uk/';
 
+require( 'dotenv' ).config();
+
 switch ( process.env.ELEVENTY_ENV ) {
 	case 'development':
 		siteurl = 'http://localhost:8080/';
@@ -10,4 +12,5 @@ module.exports = {
 	siteurl: siteurl
 	,title: 'The Classic Barber of Hertford'
 	,environment: process.env.ELEVENTY_ENV
+	,mapAPIKey: process.env.GOOGLE_MAPS_API_KEY
 };
